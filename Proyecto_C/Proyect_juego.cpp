bd915d7b6e8494c3f2a2a5cefbf2c67b7320b77f
Carlos-Camp0sec0 <132490343+Carlos-Camp0sec0@users.noreply.github.com>
2023-05-03 12:25:37 -0600

    Add files via upload

---
+++ Proyecto_C/Proyect_juego.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int jugador = 1;
+	int piezas=2,fila,piezas_q;
+	int piezas_r=3;
+	bool fin;
+	
+	while (piezas_r>1){
+		cout<<"jugador "<<jugador<<",escoja la fila: (1-3) "<<endl;
+		cin>>fila;
+		switch (fila){
+			case 1: piezas=3;
+			case 2: piezas=5;
+			case 3: piezas=7;
+			default: cout<<"numero invalido"<<endl;
+			break;
+			
+			}
+		cout<<"escoja un numero de piezas: "<<endl;
+		cin>>piezas_q;
+		piezas_r-=1;
+	}
+	
+	
+	return 0;
+}

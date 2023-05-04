7c938874e2cfbbf1b3361be9eaeacac554d0b724
CarlosMiranda89 <132486972+CarlosMiranda89@users.noreply.github.com>
2023-05-03 18:54:42 -0600

    Update Proyect_juego.cpp

--- Proyecto_C/Proyect_juego.cpp
+++ Proyecto_C/Proyect_juego.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void graficos(int x){
+	for (int i = 0; i < x; i++) {
+        cout << "# ";
+    }
+    cout << endl;
+	
+}
+
 int main(){
 	int jugador = 1;
-	int piezas=2,fila,piezas_q;
-	int piezas_r=3;
+	int fila1=3,fila2=5,fila3=7,fila,piezas,piezas_q;
+	int piezas_r=4;
+
 	bool fin;
 	
 	while (piezas_r>1){
+		graficos (fila1);
+		graficos (fila2);
+		graficos (fila3);
 		cout<<"jugador "<<jugador<<",escoja la fila: (1-3) "<<endl;
 		cin>>fila;
 		switch (fila){
-			case 1: piezas=3;
-			case 2: piezas=5;
-			case 3: piezas=7;
-			default: cout<<"numero invalido"<<endl;
+			case 1: fila1-=1;
+			case 2: fila2-=2;
+			case 3: fila3-=3;
+			
 			break;
 			
 			}

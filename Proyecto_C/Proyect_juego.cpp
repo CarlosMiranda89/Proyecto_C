86469419e4e961d8a2e8e4aa850166b7d9f70142
CarlosMiranda89 <132486972+CarlosMiranda89@users.noreply.github.com>
2023-05-11 13:50:22 -0600

    Update Proyect_juego.cpp

--- Proyecto_C/Proyect_juego.cpp
+++ Proyecto_C/Proyect_juego.cpp
@@ -11,30 +11,35 @@ void graficos(int x){
 
 int main(){
 	int jugador = 1;
-	int fila1=3,fila2=5,fila3=7,fila,piezas,piezas_q;
-	int piezas_r=4;
+	int fila1=3,fila2=5,fila3=7,fila=0
+	,piezas_q;
+	int i=4;
 
 	bool fin;
 	
-	while (piezas_r>1){
+	while (i>=1){
 		graficos (fila1);
 		graficos (fila2);
 		graficos (fila3);
+		if (i==1){
+			break;
+		}
 		cout<<"jugador "<<jugador<<",escoja la fila: (1-3) "<<endl;
 		cin>>fila;
+		piezas_q=0;
+		cout<<"escoja un numero de piezas: "<<endl;
+		cin>>piezas_q;
 		switch (fila){
-			case 1: fila1-=1;
-			case 2: fila2-=2;
-			case 3: fila3-=3;
+			case 1: fila1-=piezas_q;
+			case 2: fila2-=piezas_q;
+			case 3: fila3-=piezas_q;
 			
 			break;
 			
 			}
-		cout<<"escoja un numero de piezas: "<<endl;
-		cin>>piezas_q;
-		piezas_r-=1;
+		i-=1;
 	}
 	
-	 
-	 return 0;
+	
+	return 0;
 }
